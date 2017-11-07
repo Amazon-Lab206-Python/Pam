c18e074412a1c681c7a9b0ae73099dff252135f0
rimelp <rimelp@amazon.com>
2017-11-07 08:43:49 -0700

    updating for Nov 6

---
+++ Django/Intro/time_display/apps/timedate/views.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime
+from django.utils.crypto import get_random_string
+
+# Create your views here.
+
+def index(request):
+    context = {
+        "date" : strftime("%m-%d-%Y",gmtime()),
+        "time" : strftime("%H:%M %p", gmtime())
+    }
+    return render(request, "timedate/index.html", context)
