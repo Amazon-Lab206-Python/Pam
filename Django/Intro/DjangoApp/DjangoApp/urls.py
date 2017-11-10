e297fdf36d2762b386fa27693fb7b1c3532be1e7
rimelp <rimelp@amazon.com>
2017-11-10 07:53:54 -0700

    updating through Nov 9

--- Django/Intro/DjangoApp/DjangoApp/urls.py
+++ Django/Intro/DjangoApp/DjangoApp/urls.py
@@ -19,4 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.blogs.urls')),
+    url(r'^surveys', include('apps.surveys.urls')),
+    url(r'^users', include('apps.users.urls')),
 ]
--- Django/Intro/DjangoApp/apps/blogs/urls.py
+++ Django/Intro/DjangoApp/apps/blogs/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^blogs$', views.index),
     url(r'^new/$', views.new),
     url(r'^create/$', views.create),
     url(r'^(?P<blog_id>\d+)$', views.show),
